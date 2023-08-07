7d31f31d00129e0633ffed1e00ac634a70de834c
JayBitterman <109260489+JayBitterman@users.noreply.github.com>
2023-08-07 16:17:16 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket as s
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,44 +7,47 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        action = "None"
         if request.form.get('Park') == 'Park':
-            connect("park")
+            action = "park"
         elif request.form.get('Tattle') == 'Tattle':
-            connect("tattle")
+            action = "tattle"
+        # SOCK_STREAM = TCP
+        client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+        # Connect client to server IP and port
+        # Online Server IP
+        address, port = "3.94.59.235", 6789
+
+        client_socket.connect((address, port))
+
+        try:
+            # Send action message to server
+            client_socket.send(bytes(action, encoding='UTF-8'))
+
+        except Exception as msg:
+            print(msg)
+
+        # Listen for server tattles
+        while True:
+            try:
+                # Receive tattle from server
+                server_msg = client_socket.recv(1024).decode()
+                if not server_msg:
+                    break
+                elif server_msg == 'Meter Maid in your area! Run!':
+                    return redirect('warning')
+            except Exception as msg:
+                print(msg)
+                # Close upon error
+                client_socket.close()
+                break
     return render_template('index.html')
 
 
-def connect(action):
-    # SOCK_STREAM = TCP
-    client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-
-    # Connect client to server IP and port
-    # Online Server IP
-    address, port = "3.94.59.235", 6789
-
-    client_socket.connect((address, port))
-
-    try:
-        # Send action message to server
-        client_socket.send(bytes(action, encoding='UTF-8'))
-
-    except Exception as msg:
-        print(msg)
-
-    # Listen for server tattles
-    while True:
-        try:
-            # Receive tattle from server
-            server_msg = client_socket.recv(1024).decode()
-            if not server_msg:
-                break
-            else:
-                print(server_msg)
-        except Exception as msg:
-            print(msg)
-            # Close upon error
-            client_socket.close()
-            break
+@app.route('/warning')
+def warning():
+    return render_template('warning.html')
 
 
 if __name__ == '__main__':
